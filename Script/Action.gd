b30f3b3c9e25f40a8a8558ed1a3942b1fa869b27
theludovyc <theludovyc@gmail.com>
2020-09-02 17:16:03 +0200

    gardening and eat brownie

---
+++ Script/Action.gd
@@ -0,0 +1,17 @@
+extends Resource
+class_name Action
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+export var name := "Action"
+
+export var hunger_cost:float
+
+export var thirst_cost:float
+
+export var fatigue_cost:float
+
+export var hygiene_cost:float
+
+export var happiness_cost:float
