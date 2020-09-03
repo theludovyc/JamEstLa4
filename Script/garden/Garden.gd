42de98c22ec60ed7bcac07084f1bb7f0815e5919
Antharuu <antho.eclips@gmail.com>
2020-09-03 17:51:08 +0200

    Plantation growing & configuring

--- Script/garden/Garden.gd
+++ Script/garden/Garden.gd
@@ -1,45 +1,67 @@
 extends Node2D
 
+# Exports
 export (int, 1, 99999999) var EvaporationTime = 600
 export (Array) var Seeds = []
 
+# Nodes
 onready var Interface = $Interface
 
-var plot_obj = preload("res://Scene/garden/Plot.tscn")
-
+# Actions
 var current_action: String = ""
 
+# Watering
 var max_water: int = 50
-var water: int = 50
+var water: int = 50 setget set_water
 
+# Planting
 var seed_type
 var seed_max = 50
-var seed_count = 15
+var seed_count = 50 setget set_seed_count
 
 
 func _ready():
-	Interface.get_node("Watering").get_node("WaterLevel").max_value = max_water
 	for child in get_children():
 		if child is Plot:
 			child.get_node("Evaporation").wait_time = EvaporationTime
+	update_water_bar()
+	update_seed_bar()
 
 
-func _process(_delta):
+func set_water(value):
+	water = value
+	update_water_bar()
+
+
+func set_seed_count(value):
+	seed_count = value
+	update_seed_bar()
+
+
+func update_water_bar():
+	Interface.get_node("Watering").get_node("WaterLevel").max_value = max_water
 	Interface.get_node("Watering").get_node("WaterLevel").value = water
 	Interface.get_node("Watering").get_node("Stat").text = str(water) + "/" + str(max_water)
+
+
+func update_seed_bar():
+	Interface.get_node("Planting").get_node("SeedsLevel").max_value = seed_count
 	Interface.get_node("Planting").get_node("SeedsLevel").value = seed_count
 	Interface.get_node("Planting").get_node("Stat").text = str(seed_count) + "/" + str(seed_max)
 
 
+# On click watering can
 func wateringcan_mode():
 	toggle_curent_action("Watering")
 
 
+# On click plantation
 func seed_select():
 	seed_type = Seeds[0]  # Temporary seed for test
 	toggle_curent_action("Planting")
 
 
+# Actions manager
 func toggle_curent_action(action: String = ""):
 	if current_action == action:
 		action = ""
--- Script/garden/Plot.gd
+++ Script/garden/Plot.gd
@@ -1,21 +1,50 @@
 extends Node2D
 class_name Plot
 
+# Plot effects
 onready var Effect_Watered = $Effects/Sprinkled
 onready var Effect_Seeded = $Effects/Seeded
+
+# List of plantations
 onready var Plants = $Plants
 
+# Growing timer / speed
+onready var Growing = $Growing
+
+# Garden
 var Garden: Node2D
 
+# Current plantation sprite
+var PlantSprite: Sprite
+
+# Informations
 var has_water: bool = false
 var has_seed: bool = false
+var grow_time: int = 0
+var grow_modifier: float = 1.0
+var is_harvestable: bool = false
 
 
 func _ready():
 	Garden = get_parent()
 	Plants.visible = false
+	randomize_grow_modifier()
 
 
+func _process(_delta):
+	if Growing.is_stopped() && has_seed:
+		Growing.start()
+	if has_water:
+		Growing.wait_time = 0.75 * grow_modifier
+	else:
+		Growing.wait_time = 1 * grow_modifier
+
+
+func randomize_grow_modifier():
+	grow_modifier = rand_range(0.8, 1.2)
+
+
+# On click on plot
 func action_used():
 	if Garden.current_action != "":
 		match Garden.current_action:
@@ -25,30 +54,63 @@ func action_used():
 				planting()
 
 
+# When timer Growing is ended
+func growing():
+	if grow_time < Garden.seed_type.GrowDuration:
+		grow_time += 1
+		time_changed()
+		randomize_grow_modifier()
+	else:
+		is_harvestable = true
+
+
+# Check for growing stade
+func time_changed():
+	var grow_duration = Garden.seed_type.GrowDuration
+	var grow_steps = Garden.seed_type.GrowSteps
+
+	var step = grow_duration / grow_steps
+	var current_stade = floor(grow_time / step)
+	change_stade_to(current_stade)
+
+
+# Define the current sprite
+func change_stade_to(current_stade):
+	if PlantSprite != null:
+		current_stade -= 1
+		var sprite_size: Vector2 = PlantSprite.region_rect.size
+		PlantSprite.region_rect.position = Vector2(sprite_size.x * current_stade, 0)
+
+
+# Watering the plot
 func watering():
 	if Garden.water > 0 && ! has_water:
-		print("Watering")
 		$Evaporation.start()
 		Garden.water -= 1
 		Effect_Watered.visible = true
 		has_water = true
 
 
+# Planting seed in plot
 func planting():
 	if Garden.seed_count > 0 && Garden.seed_type != null && ! has_seed:
-		print("Planting: " + Garden.seed_type.SeedName)
+		for plant in Plants.get_children():
+			plant.visible = false
 		for plant in Garden.Seeds:
-			var plot_plant = Plants.get_node("plant")
-			if plot_plant != null:
-				if plant is Sprite && plant.name == Garden.seed_type.SeedName:
-					plot_plant.visible = true
-				plot_plant.visible = false
+			if Plants.find_node(plant.SeedName):
+				var plant_node = Plants.get_node(plant.SeedName)
+				if plant_node != null:
+					if plant.SeedName == Garden.seed_type.SeedName:
+						PlantSprite = plant_node
+						PlantSprite.visible = true
 		Plants.visible = true
 		Effect_Seeded.visible = true
 		Garden.seed_count -= 1
 		has_seed = true
+		change_stade_to(0)
 
 
+# End of timer Evaporation
 func evaporation():
 	has_water = false
 	Effect_Watered.visible = false
